dc3637548bdef859b97c746a85d8ae429c1e421f
gawa-hexi <skyla.karis@gmail.com>
2024-12-02 15:23:32 +1100

    setup negate function

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ with open(table_address, 'r') as f:
         print("Row (raw): ", line.strip())
         row = line.strip().split(' ')
         print("Row (fin): ", row, '\n')
+
+def negate_var(var: str):
+    tmp = ''
+    for e in var:
+        tmp += e + "\u0305"
+    return tmp
+# results = 4, 5
+#4 inputs + 2 results.
+
+# print SOP
+
+# if element is "0":
+    # print()
+
+test = negate_var('test')
+print(test)
+# print("AB" + "\u0305")
